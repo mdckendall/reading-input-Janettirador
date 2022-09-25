c68feb26531761c305afeda9098e8235f6ab2abe
JanetTirador <113356729+Janettirador@users.noreply.github.com>
2022-09-25 15:20:16 +0000

    Added string 2

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 class Main {
   public static void main(String[] args) {
 
-    String Str1 = new String("Word");
+String Str1 = new String("Word");
+String Str2 = new String("example");
+    
+System.out.print("String Length :");         
+System.out.println(Str1.length());
 
-    System.out.print("String Length :");
-    System.out.println(Str1.length());
+System.out.print("String Length :");
+System.out.println(Str2.length());    
 }
   }
